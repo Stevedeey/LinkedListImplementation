38d8644a1e7c0968c85b948874b80aafbce7fdca
stevedeey <stvolutosin69@gmail.com>
2021-06-09 15:33:15 +0100

    add node structure

---
+++ src/main/java/com/tosin/firstImplementation/LinkedList.java
@@ -0,0 +1,16 @@
+package com.tosin.firstImplementation;
+
+public class LinkedList {
+
+    public class Node{
+        private int data;
+        private Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        private Node first;
+        private Node last;
+    }
+}
