bf6cb09a473f1ca8e252cdd628ce6ff1ae0bef97
stevedeey <stvolutosin69@gmail.com>
2021-06-09 18:07:56 +0100

    add removeLast

--- src/main/java/com/tosin/firstImplementation/LinkedList.java
+++ src/main/java/com/tosin/firstImplementation/LinkedList.java
@@ -13,13 +13,37 @@ public class LinkedList {
         private Node first;
         private Node last;
 
-        public void addLastNode(int value){
+        public void addLastNode(int value) {
             Node node = new Node(value);
-            if(first == null){
+            if (first == null) {
                 first = last = null;
+            } else {
+                last.next = node;
+                last = node;
             }
-            last.next = node;
-            last = node;
         }
+
+        public void addFirstNode(int value){
+            Node node = new Node(value);
+            if(first==null){
+                first = last = null;
+            }else {
+            node.next = first;
+
+            first = node;
+            }
+        }
+
+        public void removeLast(){
+            var current = first;
+            while(current !=null){
+                if(current.next==last) break;
+                current = current.next;
+            }
+            current.next = null;
+            last = current;
+        }
+
+
     }
 }
